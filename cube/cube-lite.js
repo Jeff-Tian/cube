750e896242fca0df1ca5e8d0ec2b607068328c65
田杰 <jefftian@hcdlearning.com>
2016-04-09 15:41:32 +0800

    Use mapping to reflect the change.

--- cube/cube-lite.js
+++ cube/cube-lite.js
@@ -4,38 +4,50 @@ if (typeof module !== 'undefined' && module.exports) {
 
 function CubeLite(label) {
     this.label = label.split('');
-    this.changeMethod = {
-        0: 0,
-        1: 1,
-        2: 2,
-        3: 3
-    };
 
     if (!CubeLite.__initialized__) {
         CubeLite.prototype.change = function (changeDef) {
-            var self = this;
+            var another = this.label.slice(0);
 
-            function changeInner(a) {
-                var t = self.label[a[0]];
-                for (var i = 0; i < a.length - 2; i++) {
-                    self.label[a[i]] = self.label[a[i + 1]];
+            for (var source in changeDef) {
+                var target = changeDef[source];
+
+                this.label[source] = another[target];
+            }
+
+            for (var i = 0; i < 24; i++) {
+                if (typeof changeDef[i] === 'undefined') {
+                    this.label[i] = another[i];
                 }
-
-                self.label[a[a.length - 2]] = t;
             }
+        };
 
-            for (var i = 0; i < changeDef.length; i++) {
-                var a = changeDef[i];
+        CubeLite.prototype.getChangeMethod = function (label1, label2) {
+            var ret = [];
 
-                changeInner(a);
-            }
 
-            return this;
+            return ret;
         };
 
         CubeLite.prototype.F = function () {
             console.log('before F: ', this.label.toString());
-            this.change([[0, 2, 3, 1, 0], [9, 20, 14, 19, 9], [11, 21, 12, 18, 11]]);
+            // this.change([[0, 2, 3, 1, 0], [9, 20, 14, 19, 9], [11, 21, 12, 18, 11]]);
+            this.change({
+                0: 2,
+                2: 3,
+                3: 1,
+                1: 0,
+
+                9: 20,
+                20: 14,
+                14: 19,
+                19: 9,
+
+                11: 21,
+                21: 12,
+                12: 18,
+                18: 11
+            });
             console.log('after F: ', this.toString());
 
             return this;
